7a6cca29c73cae054ba5646a8cf2253d09f0e6d3
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-07-25 17:08:59 -0300

    add csv reader

--- cmd/httpmonitor/main.go
+++ cmd/httpmonitor/main.go
@@ -1,25 +1,63 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 )
 
-func main() {
-	now := time.Now()
-	url := os.Args[1]
-	get, err := http.Get(url)
+type Server struct {
+	ServerName    string
+	ServerUrl     string
+	ExecutionTime float64
+}
 
+func createServerList(data [][]string) []Server {
+	var servers []Server
+
+	for i, line := range data {
+		if i > 0 {
+			server := Server{
+				ServerName: line[0],
+				ServerUrl:  line[1],
+			}
+			servers = append(servers, server)
+		}
+
+	}
+	return servers
+}
+
+func main() {
+
+	f, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Println("error in get url")
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	elapsed := time.Since(now).Seconds()
-	status := get.StatusCode
+	defer f.Close()
 
-	fmt.Printf("Status: {%d} Time: {%f}\n", status, elapsed)
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	servers := createServerList(data)
+	for _, server := range servers {
+		now := time.Now()
+		get, err := http.Get(server.ServerUrl)
+		if err != nil {
+			fmt.Println(err)
+		}
+		server.ExecutionTime = time.Since(now).Seconds()
+		status := get.StatusCode
+		fmt.Printf("Status: [%d] Time: [%f] Url: [%s]\n", status, server.ExecutionTime, server.ServerUrl)
+	}
 
 }
